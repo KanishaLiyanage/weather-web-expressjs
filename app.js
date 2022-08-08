5568abdf1a50c8217d9a84b8a7f9527f7ed16fb4
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-08-08 14:06:47 +0530

    search option

--- app.js
+++ app.js
@@ -9,19 +9,23 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
 
     const day = date.getDay();
     const today = date.getDate();
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Colombo&appid=5f39ac098bfb1d20edb29bbc65746da8";
+    let city = "Colombo";
+    const appID = "5f39ac098bfb1d20edb29bbc65746da8";
+    const units = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + appID + "&units=" +units;
 
-    https.get(url, function(response){
+    https.get(url, function (response) {
 
         console.log(response.statusCode);
 
-        response.on("data", function(data){
+        response.on("data", function (data) {
 
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
@@ -29,27 +33,39 @@ app.get("/", function(req, res){
             const wind = weatherData.wind.speed;
             const precipitation = weatherData.main.feels_like;
             const humidity = weatherData.main.humidity;
+            // const cityName = weatherData.name;
+            const country = weatherData.sys.country;
+
+            res.render("home",
+                {
+                    kindOfDay: day,
+                    Date: today,
+                    temperature: temp,
+                    description: desc,
+                    PRECIPITATION: precipitation,
+                    HUMIDITY: humidity,
+                    WIND: wind,
+                    City: city,
+                    Country: country
+                }
+            );
 
         });
-
-        res.render("home",
-            {
-                kindOfDay: day, 
-                Date: today,
-                temperature: temp,
-                description: desc,
-                PRECIPITATION: precipitation,
-                HUMIDITY: humidity,
-                WIND: wind
-            }
-        );
 
     });
 
 });
 
+app.post("/", function(req, res){
+
+    console.log(req.body.location);
+
+    res.redirect("/");
+
+});
 
 
-app.listen(port, function(){
+
+app.listen(port, function () {
     console.log("Server started on port" + port + ".");
 });
