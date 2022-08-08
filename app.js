1a869fcdcefb201befd74808aa0e5d1bd0132a51
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-08-08 10:47:06 +0530

    folder structure updated

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const port = 3000;
 
 const app = express();
 
+app.set("view engine", "ejs");
 
-
-
+app.get("/", function(req, res){
+    res.render("home");
+});
 
 app.listen(port, function(){
     console.log("Server started on port" + port + ".");
